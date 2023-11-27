8b4fe2824fa8a6d953894ca8eb673c55fa44f825
williamzborja <williamzborja@mail.com>
2023-11-27 09:00:10 -0500

    Add default font monaspace nerd font

--- c_version/config.def.h
+++ c_version/config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 20;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "MonaspiceNe Nerd Font Propo:size=10" };
+static const char dmenufont[]       = "MonaspiceNe Nerd Font Propo:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@
 #rm /usr/share/xsessions/rwm.desktop
 ln -s $(pwd)/run /usr/bin/rwm
 ln -s $(pwd)/rwm.desktop /usr/share/xsessions/rwm.desktop
-
+ln -s $(pwd)/wp.jpg ~/.wallpaper 
